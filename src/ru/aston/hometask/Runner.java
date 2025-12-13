0c80fbf8d52c4efeadea980d68e3f39559767c97
Valson2 <93409115+Valson2@users.noreply.github.com>
2025-12-13 14:30:44 +0300

    Add files via upload

--- src/ru/aston/hometask/Runner.java
+++ src/ru/aston/hometask/Runner.java
@@ -3,6 +3,7 @@ package ru.aston.hometask;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import static ru.aston.hometask.utils.ConstantUtils.FILE_NAME;
@@ -22,7 +23,7 @@ public class Runner {
 			}
 			students.stream().peek(System.out::println).map(student -> student.getBooks())
 					.flatMap(books -> books.stream()).sorted().distinct().filter(book -> book.getReleaseDate() > 2000)
-					.limit(3).map(book -> book.getReleaseDate()).max(Integer::max)
+					.limit(3).map(book -> book.getReleaseDate()).findFirst()
 					.ifPresentOrElse(System.out::println, () -> System.out.println("Book is not found"));
 
 		} catch (FileNotFoundException e) {
